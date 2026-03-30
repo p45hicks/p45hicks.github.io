a28686fc15873f1311cacaf53c1092148653520d
Paul Hicks <paul.hicks@sentify.co>
2026-03-31 12:49:25 +1300

    Updated for workspaces, separate test dir, etc

--- packages/test/test/App.test.tsx
+++ packages/test/test/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import App from '@site/site/App'
 
 describe('App', () => {
   it('renders welcome message', () => {
