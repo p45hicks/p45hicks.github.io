397df8dda33e0edb92240f6e6159da4b12bdeb68
Paul Hicks <paul.hicks@sentify.co>
2026-04-13 21:10:17 +1200

    Add FlipCard and style Basics with it
    I also started on changes to normal Card, but this is WIP.
    Also added an editor config and changed the formatting to 2 spaces.

--- packages/cv/Cv.tsx
+++ packages/cv/Cv.tsx
@@ -2,225 +2,233 @@ import type { JSX } from 'react';
 
 import type { ResumeAwardSchema, ResumeEducationSchema, ResumeInterestSchema, ResumeProfileSchema, ResumeProjectSchema, ResumeReferenceSchema, ResumeSchema, ResumeSkillSchema, ResumeWorkSchema, } from '.';
 import { ResumeProvider, useResume } from './resumeHooks';
-import { Card, CardHeader, CardContent } from '@paul-hicks-nz/elements/Card';
+import { Card, CardHeader, CardContent, FlipCard, FlipCardFront, FlipCardBack } from '@paul-hicks-nz/elements/Card';
 
 const yearOnly: Intl.DateTimeFormatOptions = { year: 'numeric' };
 function formatYear(dateString?: string): string {
-    if (dateString === undefined) {
-        return '';
-    }
-    const date = new Date(dateString);
-    if (isNaN(date.getTime())) {
-        // The standard states that if a date string is invalid,
-        // the Date constructor should return an "Invalid Date" object,
-        // which has a getTime() method that returns NaN.
-        // If this happens, we just return the original string.
-        // A reasonable use cases for this:
-        // - if the date string is just a year, e.g. "2020",
-        //    which is valid according to the schema but not a valid
-        //    date string for the Date constructor. 
-        // - if the date string is "now" or "present", which are commonly
-        //   used in resumes to indicate current employment, but are not
-        //   valid date strings for the Date constructor.
-        return dateString;
-    }
-    return date.toLocaleDateString(undefined, yearOnly);
+  if (dateString === undefined) {
+    return '';
+  }
+  const date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    // The standard states that if a date string is invalid,
+    // the Date constructor should return an "Invalid Date" object,
+    // which has a getTime() method that returns NaN.
+    // If this happens, we just return the original string.
+    // A reasonable use cases for this:
+    // - if the date string is just a year, e.g. "2020",
+    //    which is valid according to the schema but not a valid
+    //    date string for the Date constructor. 
+    // - if the date string is "now" or "present", which are commonly
+    //   used in resumes to indicate current employment, but are not
+    //   valid date strings for the Date constructor.
+    return dateString;
+  }
+  return date.toLocaleDateString(undefined, yearOnly);
 }
 
 export function CV({ cv }: { cv: ResumeSchema }): JSX.Element {
-    return (
-        <ResumeProvider resume={cv}>
-            <>
-                <BasicInfo />
-                <Socials />
-                <WorkExperience />
-                <References />
-                <Projects />
-                <Skills />
-                <Interests />
-                <Education />
-                <Awards />
-            </>
-        </ResumeProvider>
-    );
+  return (
+    <ResumeProvider resume={cv}>
+      <>
+        <BasicInfo />
+        <Socials />
+        <WorkExperience />
+        <References />
+        <Projects />
+        <Skills />
+        <Interests />
+        <Education />
+        <Awards />
+      </>
+    </ResumeProvider>
+  );
 }
 
 export function BasicInfo(): JSX.Element {
-    const resume = useResume();
-    return (
-        <Card className='BasicInfo'>
-            <CardHeader title={resume.basics?.name ?? 'Basic Info'}>
-                <div>
-                    <a href={resume.basics?.url}>
-                        <img className='w-auto' src={resume.basics?.image} alt={resume.basics?.name ?? 'Profile image'} />
-                    </a>
-                </div>
-            </CardHeader>
-            <CardContent>
-                <>
-                    <div>{resume.basics?.label}</div>
-                    <div>{resume.basics?.email}</div>
-                    <div>{resume.basics?.phone}</div>
-                </>
-            </CardContent>
-        </Card>
-    );
+  const resume = useResume();
+  return (
+    <FlipCard className='BasicInfo h-30 w-100 m-2'>
+      <FlipCardFront>
+        <div className="grid grid-cols-2 grid-rows-1 m-2">
+          <div>
+            <div className='text-2xl font-bold'>{resume.basics?.name}</div>
+            <div>{resume.basics?.label}</div>
+          </div>
+          <img className='h-25 col-2 place-self-center' src={resume.basics?.image} alt={resume.basics?.name ?? 'Profile image'} />
+        </div>
+      </FlipCardFront>
+      <FlipCardBack>
+        <div className="grid grid-cols-2 grid-rows-1 m-2">
+          <div>
+            <div className='text-2xl font-bold'>{resume.basics?.name}</div>
+            <div className='col-1'>{resume.basics?.email}</div>
+            <div className='col-1'>{resume.basics?.phone}</div>
+          </div>
+          <img className='h-25 col-2 place-self-center' src={resume.basics?.image} alt={resume.basics?.name ?? 'Profile image'} />
+        </div>
+      </FlipCardBack>
+    </FlipCard>
+  );
 }
 
 export function Socials(): JSX.Element {
-    const resume = useResume();
-    return (
-        <Card className='Socials'>
-            <>
-                <h1>Socials</h1>
-                {resume.basics?.profiles?.map((profile: ResumeProfileSchema, index: number) => (
-                    <div key={`profile-${index}`}>
-                        <a href={profile.url}>{profile.network}</a>
-                    </div>
-                ))}
-            </>
-        </Card>
-    );
+  const resume = useResume();
+  return (
+    <Card className='Socials'>
+      <CardHeader title='Socials' />
+      <CardContent>
+        {resume.basics?.profiles?.map((profile: ResumeProfileSchema, index: number) => (
+          <div key={`profile-${index}`}>
+            <a href={profile.url} target="_blank" rel="noopener noreferrer">{profile.network}</a>
+          </div>
+        )) || []}
+      </CardContent>
+    </Card>
+  );
 }
 
 export function WorkExperience(): JSX.Element {
-    const resume = useResume();
-    return (
-        <Card className='WorkExperience'>
-            <>
-                <h1>Experience</h1>
-                {resume.work?.map((work: ResumeWorkSchema, index: number) => (
-                    <div key={`work-${index}`}>
-                        <a href={work.url}>{work.name}</a>
-                        <div>{work.position}</div>
-                        <div className='text-sm'><em>{formatYear(work.startDate)} - {formatYear(work.endDate)}</em></div>
-                        <div>{work.summary}</div>
-                        <ul>
-                            {work.highlights?.map((highlight: string, highlightIndex: number) => (
-                                <li key={`work-${index}-highlight-${highlightIndex}`}>{highlight}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-            </>
-        </Card>
-    );
+  const resume = useResume();
+  return (
+    <Card className='WorkExperience'>
+      <CardHeader title='Experience' />
+      <CardContent>
+        {resume.work?.map((work: ResumeWorkSchema, index: number) => (
+          <div key={`work-${index}`}>
+            <a href={work.url}>{work.name}</a>
+            <div>{work.position}</div>
+            <div className='text-sm'><em>{formatYear(work.startDate)} - {formatYear(work.endDate)}</em></div>
+            <div>{work.summary}</div>
+            <ul>
+              {work.highlights?.map((highlight: string, highlightIndex: number) => (
+                <li key={`work-${index}-highlight-${highlightIndex}`}>{highlight}</li>
+              ))}
+            </ul>
+          </div>
+        )) || []}
+      </CardContent>
+    </Card>
+  );
 }
 
 export function Projects(): JSX.Element {
-    const resume = useResume();
-    return (
-        <Card className='Projects'><>
-            <h1>Projects</h1>
-            {resume.projects?.map((project: ResumeProjectSchema, index: number) => (
-                <div key={`project-${index}`}>
-                    <a href={project.url}>{project.name}</a>
-                    <div>{formatYear(project.startDate)} - {formatYear(project.endDate)}</div>
-                    <div>{project.description}</div>
-                    <ul className='list-disc list-inside'>
-                        {project.roles?.map((role: string, roleIndex: number) => (
-                            <li key={`project-${index}-${role}-${roleIndex}`}>{role}</li>
-                        ))}
-                    </ul>
-                    {
-                        project.highlights?.map((highlight: string, highlightIndex: number) => (
-                            <div key={`project-${index}-highlight-${highlightIndex}`}>{highlight}</div>
-                        ))
-                    }
-                </div>
-            ))
+  const resume = useResume();
+  return (
+    <Card className='Projects'>
+      <CardHeader title='Projects' />
+      <CardContent>
+        {resume.projects?.map((project: ResumeProjectSchema, index: number) => (
+          <div key={`project-${index}`}>
+            <a href={project.url}>{project.name}</a>
+            <div>{formatYear(project.startDate)} - {formatYear(project.endDate)}</div>
+            <div>{project.description}</div>
+            <ul className='list-disc list-inside'>
+              {project.roles?.map((role: string, roleIndex: number) => (
+                <li key={`project-${index}-${role}-${roleIndex}`}>{role}</li>
+              ))}
+            </ul>
+            {
+              project.highlights?.map((highlight: string, highlightIndex: number) => (
+                <div key={`project-${index}-highlight-${highlightIndex}`}>{highlight}</div>
+              ))
             }
-        </></Card>
-    );
+          </div>
+        )) || []}
+      </CardContent>
+    </Card>
+  );
 }
 
 export function Education(): JSX.Element {
-    const resume = useResume();
-    return (
-        <Card className='Education'><>
-            <h1>Education</h1>
-            {resume.education?.map((edu: ResumeEducationSchema, index: number) => (
-                <div key={`education-${index}`}>
-                    <a href={edu.url}>{edu.studyType} {edu.area && `(${edu.area})`} at {edu.institution}</a>
-                    <div className="text-sm"><em>{formatYear(edu.startDate)} - {formatYear(edu.endDate)}</em></div>
-                    <div><em>{edu.score}</em></div>
-                    <ul className='text-sm text-indent-2'>
-                        {edu.courses?.map((course: string, courseIndex: number) => (
-                            <li key={`education-${index}-course-${courseIndex}`}>{course}</li>
-                        ))}
-                    </ul>
-                </div>
-            ))
-            }
-        </></Card>
-    );
+  const resume = useResume();
+  return (
+    <Card className='Education'>
+      <CardHeader title='Education' />
+      <CardContent>
+        {resume.education?.map((edu: ResumeEducationSchema, index: number) => (
+          <div key={`education-${index}`}>
+            <a href={edu.url}>{edu.studyType} {edu.area && `(${edu.area})`} at {edu.institution}</a>
+            <div className='text-sm'><em>{formatYear(edu.startDate)} - {formatYear(edu.endDate)}</em></div>
+            <div><em>{edu.score}</em></div>
+            <ul className='text-sm text-indent-2'>
+              {edu.courses?.map((course: string, courseIndex: number) => (
+                <li key={`education-${index}-course-${courseIndex}`}>{course}</li>
+              ))}
+            </ul>
+          </div>
+        )) || []}
+      </CardContent>
+    </Card>
+  );
 }
 
 export function Skills(): JSX.Element {
-    const resume = useResume();
-    return (
-        <Card className='Skills'>
-            <>
-                <h1>Skills</h1>
-                {resume.skills?.map((skill: ResumeSkillSchema, index: number) => (
-                    <div key={`skill-${index}`}>
-                        <p>{skill.level} {skill.name}</p>
-                        <p><em>{skill.keywords?.join(', ')}</em></p>
-                    </div>
-                ))}
-            </>
-        </Card>
-    );
+  const resume = useResume();
+  return (
+    <Card className='Skills'>
+      <CardHeader title='Skills' />
+      <CardContent>
+        {resume.skills?.map((skill: ResumeSkillSchema, index: number) => (
+          <div key={`skill-${index}`}>
+            <p>{skill.level} {skill.name}</p>
+            <p><em>{skill.keywords?.join(', ')}</em></p>
+          </div>
+        )) || []}
+      </CardContent>
+    </Card>
+  );
 }
 
 export function Interests(): JSX.Element {
-    const resume = useResume();
-    return (
-        <Card className='Interests'>
-            <>
-                <h1>Interests</h1>
-                {resume.interests?.map((interest: ResumeInterestSchema, index: number) => (
-                    <div key={`interest-${index}`}>
-                        <p>{interest.name}</p>
-                        <p><em>{interest.keywords?.join(', ')}</em></p>
-                    </div>
-                ))
-                }
-            </>
-        </Card>
-    );
+  const resume = useResume();
+  return (
+    <Card className='Interests'>
+      <CardHeader title='Interests' />
+      <CardContent>
+        {resume.interests?.map((interest: ResumeInterestSchema, index: number) => (
+          <div key={`interest-${index}`}>
+            <p>{interest.name}</p>
+            <p><em>{interest.keywords?.join(', ')}</em></p>
+          </div>
+        )) || []}
+      </CardContent>
+    </Card>
+  );
 }
 
 export function References(): JSX.Element {
-    const resume = useResume();
+  const resume = useResume();
 
-    return (
-        <Card className='References'>
-            <>
-                <h1>References</h1>
-                {resume.references?.map((ref: ResumeReferenceSchema, index: number) => (
-                    <div key={`reference-${index}`}>
-                        <p>{ref.reference}</p>
-                        <p>- <em>{ref.name}</em></p>
-                    </div>
-                ))}
-            </>
-        </Card>
-    );
+  return (
+    <Card className='References'>
+      <CardHeader title='References' />
+      <CardContent>
+        {resume.references?.map((ref: ResumeReferenceSchema, index: number) => (
+          <div key={`reference-${index}`}>
+            <p>{ref.reference}</p>
+            <p>- <em>{ref.name}</em></p>
+          </div>
+        )) || []}
+      </CardContent>
+    </Card>
+  );
 }
 
 export function Awards(): JSX.Element {
-    const resume = useResume();
+  const resume = useResume();
 
-    return (
-        <Card className='Awards'><>
-            <h1>Awards</h1>
-            {resume.awards?.map((award: ResumeAwardSchema, index: number) => (
-                <div key={`award-${index}`}>
-                    <p>{award.title} from {award.awarder}</p>
-                    <p><em>{formatYear(award.date)}</em></p>
-                    <p>{award.summary}</p>
-                </div>
-            ))}
-        </></Card>);
+  return (
+    <Card className='Awards'>
+      <CardHeader title='Awards' />
+      <CardContent>
+        {resume.awards?.map((award: ResumeAwardSchema, index: number) => (
+          <div key={`award-${index}`}>
+            <p>{award.title} from {award.awarder}</p>
+            <p><em>{formatYear(award.date)}</em></p>
+            <p>{award.summary}</p>
+          </div>
+        )) || []}
+      </CardContent>
+    </Card>);
 }
--- packages/elements/Card.tsx
+++ packages/elements/Card.tsx
@@ -1,28 +1,117 @@
 import type { JSX } from 'react';
+import classNames from 'classnames';
 
-export function Card({ className, children }: { className: string, children: JSX.Element | JSX.Element[] }): JSX.Element {
-    return (
-        <div className={`${className} grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 p-4
-         bg-gray-400 text-black dark:bg-gray-800 dark:text-gray-300
-         border-2 rounded-lg border-gray-300 dark:border-black`}>
-            {children}
-        </div>
-    );
+/**
+ * 
+ * This file contains some basic card components that can be used to build the CV.
+ * The styling is basic and can be easily overridden by passing in a className prop.
+ * The components are designed to be flexible and can be used in a variety of ways.
+ */
+
+
+interface CardProps {
+  className: string;
+  children: JSX.Element | JSX.Element[];
 }
 
-export function CardHeader({ title, children }: { title: string, children?: JSX.Element | JSX.Element[] }): JSX.Element {
-    return (
-        <div>
-            <h1 className='col-span-1 text-2xl font-bold'>{title}</h1>
-            {children}
-        </div>
-    );
+/**
+ * The Card component is a container that can be used to group related content together.
+ * It is reactive and will adjust its layout based on the screen size.
+ * It uses CSS grid to create a responsive layout that can adapt to different screen sizes.
+ * 
+ * @param props The props for the Card component, which include a className for styling and children to be rendered inside the card. 
+ * @returns The React element representing the Card component.
+ */
+export function Card({ className, children }: CardProps): JSX.Element {
+  return (
+    <div className={classNames(className, 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 p-4',
+      'bg-gray-400 text-black dark:bg-gray-800 dark:text-gray-300',
+      'border-2 rounded-lg border-gray-300 dark:border-black')}>
+      {children}
+    </div>
+  );
 }
 
-export function CardContent({ children }: { children: JSX.Element | JSX.Element[] }): JSX.Element {
-    return (
-        <div className='md:col-span-2 lg:col-span-4 grid-cols-subgrid md:grid-cols-3'>
-            {children}
-        </div>
-    );
+interface CardContentProps {
+  children: JSX.Element | JSX.Element[];
+}
+/**
+ * The CardContent component is used to display the main content of the card.
+ *
+ * @param props The props for the CardContent component, which include children to be rendered inside the card content area. 
+ * @returns The React element representing the Card component.
+ */
+export function CardContent({ children }: CardContentProps): JSX.Element {
+  return (
+    <div className='md:col-span-2 lg:col-span-4 grid-cols-subgrid md:grid-cols-3'>
+      {children}
+    </div>
+  );
+}
+
+/**
+ * The FlipCard component is a more complex component that can be used
+ * to create a card that flips to reveal additional content on the back. 
+ * It takes two sets of children: frontChildren and backChildren,
+ * which are displayed on the front and back of the card, respectively.
+ *
+ * @param props The props for the FlipCard component, which include a
+ *  className for styling, and children to be displayed on the card.
+ *  Flipping can be manually configured by adding the correct classes to
+ *  the children, or by using the FlipCardFront and FlipCardBack components.
+ * @returns 
+ */
+export function FlipCard({ className, children }: CardProps): JSX.Element {
+  return (
+    <div className={classNames(className,
+      'group relative perspective-normal cursor-pointer'
+    )}>
+      {children}
+    </div>
+  );
+}
+
+export function FlipCardFront({ children }: CardContentProps): JSX.Element {
+  return (
+    <div className={classNames('absolute inset-0 h-full w-full',
+      'bg-gray-400 text-black dark:bg-gray-800 dark:text-gray-300',
+      'border-2 rounded-lg border-gray-300 dark:border-black',
+      'backface-hidden',
+      'transition-transform duration-500 transform-style-preserve-3d',
+      'group-active:-rotate-y-180')}>
+      {children}
+    </div>
+  );
+}
+export function FlipCardBack({ children }: CardContentProps): JSX.Element {
+  return (
+    <div className={classNames('absolute inset-0 h-full w-full',
+      'bg-gray-400 text-black dark:bg-gray-800 dark:text-gray-300',
+      'border-2 rounded-lg border-gray-300 dark:border-black',
+      'backface-hidden',
+      'transition-transform duration-500 transform-style-preserve-3d',
+      'not-group-active:rotate-y-180')}>
+      {children}
+    </div>
+  );
+}
+
+interface CardHeaderProps {
+  title: string;
+  children?: JSX.Element | JSX.Element[];
+}
+/**
+ * The CardHeader component is used to display a title and optional content in the header of the card.
+ * This component works with the Card (reactive width) and the FlipCard (fixed width) components, and will adjust its layout accordingly.
+ *
+ * @param props The props for the CardHeader component, which include a title to be displayed in the header and optional children to be rendered alongside the title.
+ * @returns The React element representing the Card component.
+ */
+export function CardHeader({ title, children }: CardHeaderProps): JSX.Element {
+  return (
+    <div>
+      <h1 className='col-span-1 text-2xl font-bold'>{title}</h1>
+      {children}
+    </div>
+  );
 }
