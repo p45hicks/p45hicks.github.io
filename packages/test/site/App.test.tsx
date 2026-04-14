16cda6497126d0714da1ac020d959abb2bedde97
Paul Hicks <p45hicks@gmail.com>
2026-04-14 16:41:02 +1200

    Resolve heaps of lint and test problems

--- packages/test/site/App.test.tsx
+++ packages/test/site/App.test.tsx
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'bun:test';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
-import App from '@site/site/App';
+import App from '@p45hicks/site/App';
 
 describe('App', () => {
   it('renders welcome message', () => {
